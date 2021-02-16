bc38668811294087f85795b9cd714214d5b1d803
Bruno Laviso <bruno.laviso@hotmail.com>
2021-02-16 18:58:07 -0300

    refactor: remove break lines

--- src/pages/top10.tsx
+++ src/pages/top10.tsx
@@ -2,40 +2,39 @@ import { GetStaticProps } from "next";
 import { Title } from "../styles/pages/Home";
 
 interface IProducts {
-    id: string;
-    title: string;
-  }
-  
-  interface Top10Props {
-    products: IProducts[];
-  }
+  id: string;
+  title: string;
+}
+
+interface Top10Props {
+  products: IProducts[];
+}
 
 export default function Top10({ products }: Top10Props) {
-    return (
-
-        <section>
-        <Title>Top10</Title>
-        <ul>
-          {products.map(product => {
-              return (
-                  <li key={product.id}>
-                {product.title}
-              </li>
-            )
+  return (
+    <section>
+      <Title>Top10</Title>
+      <ul>
+        {products.map(product => {
+          return (
+            <li key={product.id}>
+              {product.title}
+            </li>
+          )
         })}
-        </ul>
+      </ul>
     </section>
-    )
+  )
 }
 
 export const getStaticProps: GetStaticProps<Top10Props> = async (context) => {
-    const response = await fetch('http://localhost:3333/products');
-    const products = await response.json();
+  const response = await fetch('http://localhost:3333/products');
+  const products = await response.json();
 
-    return {
-        props: {
-            products,
-        },
-        revalidate: 5,
-    }
+  return {
+    props: {
+      products,
+    },
+    revalidate: 5,
+  }
 }
